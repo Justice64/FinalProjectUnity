31d5bb09ed311140ad343983d112039d84e62df4
Justice64 <rhuntley148@gmail.com>
2025-04-29 20:19:51 -0400

    Everything

---
+++ My project (4)/Assets/RhythmFight/Scripts/NoteController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteController : MonoBehaviour
+{
+
+    public GameObject[] notePrefabs; // References to the note prefabs that will be spawned.
+    public Vector3[] spawnPositions; // The positions where the notes will spawn.
+    public float beatTempo;
+    public string parentObjectName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) // Check if the left arrow key is pressed.
+        {
+            SpawnNote(0); // Call the SpawnNote function and pass in the index of the first note prefab.
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow)) // Check if the down arrow key is pressed.
+        {
+            SpawnNote(1); // Call the SpawnNote function and pass in the index of the second note prefab.
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow)) // Check if the up arrow key is pressed.
+        {            
+            SpawnNote(2); // Call the SpawnNote function and pass in the index of the third note prefab.
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow)) // Check if the right arrow key is pressed.
+        {
+            SpawnNote(3); // Call the SpawnNote function and pass in the index of the fourth note prefab.
+        }
+
+        transform.position -= new Vector3(0f, beatTempo * Time.deltaTime, 0f);
+    }
+    private void SpawnNote(int index)
+    {
+        GameObject parentObject = GameObject.Find("NoteHolder");
+        GameObject spawnedNote = Instantiate(notePrefabs[index], spawnPositions[index], Quaternion.identity); // Spawn the specified note prefab at the specified position.
+        spawnedNote.transform.parent = parentObject.transform;
+    }
+}
